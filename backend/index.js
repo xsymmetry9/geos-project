4b6fa4bba7ed9166499faec339e2930dbd6cfa8c
xsymmetry9 <glswim@gmail.com>
2024-11-15 16:44:30 +0800

    Update code

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const router = require("./routes/routes.js");
 
+const corsConfig ={
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use("/", router);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
+app.listen(port, () => {console.log(`CORS-enabled web server listening on port ${port}`)});
