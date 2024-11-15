83d6999ad0bf40096d232c1036cbb24ea00747b5
xsymmetry9 <glswim@gmail.com>
2024-11-15 16:41:12 +0800

    Update code

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,11 @@
+import React, {useState} from "react";
+import geosImg from "../assets/images/logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil, faSearch, faPrint } from "@fortawesome/free-solid-svg-icons";
+
+
 const Header = ({language}) =>{
+    const getLanguage = language;
     const title = 
     {
         "english": ["English", "Chinese", "Korean", "Japanese"],
@@ -6,13 +13,44 @@ const Header = ({language}) =>{
         "korean": ["영어", "중국어", "한국어", "일본어"],
         "japanese": ["えいご", "ちゅうごくご", "かんこくご", "にほんご"]
     }
-    
 
+    const langCode = () =>{
+        switch(getLanguage.toLowerCase()){
+            case "english":
+                return `/`;
+            case "chinese":
+                return `ch/`
+            case "korean":
+                return `kor/`
+            case "japanese":
+                return `jp/`
+            default:
+                return `/`;
+        }
+
+    }
     return(
         <>
             <header>
-                <h1>GEOS</h1>
+                <div className="header-container">
+                    <div className="header-image">
+                        <a href="https://online.geos.com.tw/zh-TW/home/home">
+                            <img className = "logo" src = {geosImg} alt="Geos logo" />
+                        </a>
+                    </div>
+                </div>
                 <nav>
+                <ul className='navigation-container'>
+                    <li className='navigation-lists' id="input">
+                        <a href= {`${langCode()}input`} className="nav-icon-container"><FontAwesomeIcon icon={faPencil}></FontAwesomeIcon><span className="hidden">input</span></a>
+                        </li>
+                    <li className='navigation-lists' id="preview">
+                        <a href={`${langCode()}preview`} className="nav-icon-container">
+                            <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon><span className="hidden">preview</span></a></li>
+                    <li className="navigation-lists" id="print">
+                        <a href={`${langCode()}print`} className="nav-icon-container"><FontAwesomeIcon icon={faPrint}></FontAwesomeIcon><span className="hidden">print</span></a></li>
+                    <li>languages</li>
+                </ul>
                     <ul>
                         <li><a href="/">{title[language.toLowerCase()][0]}</a></li>
                         <li><a href="/ch">{title[language.toLowerCase()][1]}</a></li>
