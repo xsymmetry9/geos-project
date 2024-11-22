00857c1d40a7f9336819a3d20a645660b6f56108
xsymmetry9 <glswim@gmail.com>
2024-11-22 21:09:16 +0800

    Update file

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,12 @@
+import "../styles/components/footer.scss";
+
 const Footer = () =>{
     const date = new Date().getFullYear();
     return(
         <footer>
-            <div className="footer-content">
-                <p>&copy; {date} GEOS.  All rights reserved. </p>
+            <div className='footer-container'>
+                <p>&copy; 2024 GEOS property</p>
+                <p>Created by Gary</p>
             </div>
         </footer>
     )
