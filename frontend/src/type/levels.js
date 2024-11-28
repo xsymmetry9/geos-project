cf9e5a0972678abf83bdae669bc6992d15bc45e0
xsymmetry9 <glswim@gmail.com>
2024-11-28 14:22:28 +0800

    Add folders

---
+++ frontend/src/type/levels.js
@@ -0,0 +1,16 @@
+class Levels{
+    constructor(
+        name,
+        initial,
+        target,
+        final
+    )
+    {   
+        this.name = name;
+        this.initial = initial;
+        this.target = target;
+        this.final = final;
+    }
+
+}
+export default Levels;
---
+++ frontend/src/type/student.js
@@ -0,0 +1,47 @@
+import levels from './levels';
+import {format} from "date-fns"
+
+class Student{
+    constructor(
+        name,
+        course,
+        textbook, 
+        attendance, 
+        totalLessons,
+        levels, //It's an object
+        comment)
+    {
+        this.name = name;
+        this.course = course;
+        this.textbook = textbook;
+        this.attendance = attendance;
+        this.totalLessons = totalLessons;
+        this.levels = levels;
+        this.comment= comment;
+        this.average = this.getAverage();
+    }
+
+    getPercentage = () =>{
+        this.average = Math.round((this.attendance / this.totalLessons) * 100);
+        return this.average;
+    }
+
+    getDate = () =>{
+        return format(new Date(), "MM/dd/yyyy");
+    }
+
+    getTotal = (category) =>{
+        let sum = 0;
+        this.levels.forEach((item) =>{
+            sum += item[category]
+        })
+        return sum;
+    }
+    getAverage =(category) =>{
+        return this.getTotal(category) / this.levels.length;
+    }
+
+
+}
+
+export default Student;
