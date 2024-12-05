b828862ccc055041b1e0981a14a3e82bb15ad684
xsymmetry9 <glswim@gmail.com>
2024-12-05 16:41:08 +0800

    Add file

---
+++ frontend/src/components/Form/components/Pagination.jsx
@@ -0,0 +1,27 @@
+const Pagination = () =>{
+    return(
+        <div className="root-pagination">
+            <div className="pagination-container">
+                <div className="pagination-wrapper">
+                    <div className="circle">1</div>
+                    <p>Info</p>
+                </div>
+                <div className="pagination-wrapper">
+                    <div className="circle">2</div>
+                    <p>Level</p>
+                </div>
+                <div className="pagination-wrapper">
+                    <div className="circle">3</div>
+                    <p>Feedback</p>
+                </div>
+                <div className="pagination-wrapper">
+                    <div className="circle">4</div>
+                    <p>Done</p>
+                </div>
+         
+            </div>
+        </div>
+    )
+}
+
+export default Pagination
---
+++ frontend/src/components/Form/form_languages.js
@@ -0,0 +1,37 @@
+const form_personal_information = {
+    "english": {
+        title: "Personal Information",
+        input_name: "name",
+        input_course: "course",
+        input_textbook: "textbook",
+        input_attendance: "attendance",
+        input_totallessons: "total lessons"
+    },
+    "chinese":{
+        title: "個人資訊",
+        input_name: "姓名",
+        input_course: "課程",
+        input_textbook: "教科書",
+        input_attendance: "出席",
+        input_totallessons: "總課程數"
+    },
+    "korean":{
+        title: "개인 정보",
+        input_name: "이름",
+        input_course: "과목",
+        input_textbook: "교과서",
+        input_attendance: "출석",
+        input_totallessons: "전체 수업"
+        
+    },
+    "japanese":{
+        title: "個人情報",
+        input_name: "名前",
+        input_course: "コース",
+        input_textbook: "教科書",
+        input_attendance: "出席",
+        input_totallessons: "総レッスン数",
+    }
+}
+
+export default form_personal_information;
