25c75f59b01fcd397eed49cee545a7c7c82886f6
xsymmetry9 <glswim@gmail.com>
2025-03-11 15:10:44 +0800

    Chore

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,29 +1,6 @@
 import React from "react";
 import data from "../../public/levelInformation.json";
 
-const toBeSorted = [
-  {"level": "1", "description": "Has the ability to use common, introductory words and phrases to express oneself."},
-  {"level": "2", "description": "Has fundamental vocabulary of individual terms and phrases about specific, everyday situations."},
-  {"level": "3", "description": "Possesses rudimentary vocabulary terms and phrases…onal information and fundamental practical needs."},
-  {"level": "4", "description": "Demonstrates satisfactory vocabulary with fundamen… communication and essential survival situations."},
-  {"level": "5", "description": "Possesses solid vocabulary for handling regular, everyday interactions about common subjects."},
-  {"level": "6", "description": "Show a satisfactory vocabulary foundation to commu…avel and recent news with some rephrasing needed."},
-  {"level": "7", "description": "Exhibits functional, practical vocabulary and can …topics and commonplace events with relative ease."},
-  {"level": "8", "description": "Uses much of the specific and natural language acr…xpertise within the domain has been sought after."},
-  {"level": "9", "description": "Maintains a strong grasp of key technical terms an… both specialized and general, everyday subjects."},
-  {"level": "10", "description": "Exhibits a rich lexicon that enables speaking and …d utilizing language with precision and accuracy."},
-  {"level": "1.5", "description": "Has a growing vocabulary of daily, common words an…ressions that can be used in everyday situations."},
-  {"level": "2.5", "description": "Has a continuously expanding basic set of words and phrases for commonplace, everyday situations."},
-  {"level": "3.5", "description": "Displays a foundational vocabulary for addressing …rds having sufficient knowledge in these aspects."},
-  {"level": "4.5", "description": "Adequately addresses fundamental communication and…ntermediate interactions about familiar subjects."},
-  {"level": "5.5", "description": "Has adequate vocabulary for dealing with familiar …lopment for a broader range of daily life topics."},
-  {"level": "6.5", "description": "Is capable of handling regular, familiar  interact…ics with the aim of achieving greater competency."},
-  {"level": "7.5", "description": "Has developed a broad vocabulary for familiar topi…l field and other specialized fields of interest."},
-  {"level": "8.5", "description": "Demonstrates the ability to use much of the specif…other unfamiliar subjects, specialized and broad."},
-  {"level": "9.5", "description": "Currently improving upon using less descriptive la… when using vocabulary across all subject matter."},
-  {"level": "10+", "description": "Has a sophisticated lexical range and verbal arsen…atural language, colloquial expressions and slang"}
-];
-
 function Test() {
 
   const convertLevel = (level) => {
