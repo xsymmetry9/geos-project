46ac53d51e3271c5a13aaf9ce31bfd4e1eeb1164
xsymmetry9 <glswim@gmail.com>
2025-03-21 21:23:37 +0800

    Chore

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,13 @@ const cors = require("cors");
 const router = require("./routes/routes.js");
 
 const corsConfig ={
-    origin: "http://localhost:5173",
+    origin: "http://localhost:3000",
     optionsSuccessStatus: 200
 }
 app.use(cors(corsConfig));
 app.use(express.json());
 app.use("/", router);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {console.log(`CORS-enabled web server listening on port ${port}`)});
