6004e29e42bfeb91bb90efb3888049b5f8c15bfe
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-05-23 11:08:09 +0800

    Remove password

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,11 +5,8 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./routes/routes.js");
 const session = require('express-session');
-const passport = require('passport');
-const initialializePassport = require('./config/passportConfig.js');
 
 const app = express();
-initialializePassport(passport);
 
 // CORS Configuration
 const corsConfig = {
@@ -30,11 +27,6 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
   }
 }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-initialializePassport(passport);
 
 // Routes
 app.use("/", router);
