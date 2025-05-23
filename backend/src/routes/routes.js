3437be913adf82a97125f288c187e8d534a3e045
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-05-23 11:09:52 +0800

    Chore: login function

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -10,11 +10,13 @@ const {
     getTeacherByEmailOnDB,
     createTeacher,
     loginTeacher} = require("../controllers/adminController.js");
-const {ensureNotAuthenticated, ensureAuthenticated} = require("../middlewares/authMiddleware.js");
+const {verifyToken} = require("../middlewares/authMiddleware.js");
 
 const levelDataPath = path.join(__dirname, "../../asset/levelInformation.json");
-const passport = require('passport');
 
+router.get("/api/verify-token", verifyToken, (req, res) => {
+    res.json({authenticate: true, user: req.user});
+})
 //Verify admin
 router.post("/api/admin/login", loginAdmin);
 
@@ -28,33 +30,25 @@ router.get("api/admin/teachers/language", getTeachersByLanguageOnDB );
 router.get("/api/getTeacherByEmail", getTeacherByEmailOnDB);
 
 // Member Login
-router.post("/api/login", ensureNotAuthenticated, loginTeacher); //Blocks logged-users from login page
+router.post("/api/login", loginTeacher); //Blocks logged-users from login page
 
 // Current Member
 router.get("/api/currentTeacher", (req, res) =>{
-    if(req.isAuthenticated()) {
-        return res.json({user: req.user});
-    } else {
-        return res.status(401).json({user: null});
-    }
+
 })
 
-//Logout route
-router.post("/api/logout", (req, res) => {
-    req.logout((err) => {
-        if(err) return res.status(500).json({message: "Error logging out!"});
-        req.session.destroy(() => {
-            res.clearCookie('connect.sid');
-            return res.json({status: true, message: "Logged out"});
-        });
-    })
-})
 // Profile
-router.post("/api/profile", ensureAuthenticated, (req, res) => {
-    res.json({
-        message: "This is a protected route",
-        user: "Some member"
+router.post("/api/profile", verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+            authData
+            })
+        }
     })
+ 
 })
 //Gets all users
 router.get("/api", async (req, res) =>{
