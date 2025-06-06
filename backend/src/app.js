a2e2df990d86ffc0c36bf3b5b2b0d3ad451a51f5
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-06-06 11:14:57 +0800

    Chore: work on login page

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 // app.js
 const dotenv = require("dotenv");
-dotenv.config({path: `.env${process.env.NODE_ENV || 'development'}`});
+dotenv.config({path: `.env.${process.env.NODE_ENV || 'development'}`});
 
 const express = require("express");
 const cors = require("cors");
