5490b156be22ddb23cf3736ddafd6be1df34e754
xsymmetry9 <glswim@gmail.com>
2025-07-14 14:56:31 +0800

    Fix border

--- frontend/src/components/PrintComponents/Legend.tsx
+++ frontend/src/components/PrintComponents/Legend.tsx
@@ -4,14 +4,14 @@ const Legend = () => {
     console.log(legend.english);
 
     return(
-        <caption className="border-l border-r border-slate-500 text-gray-800 text-[8px]">
-            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center p-[2px]">
+        <caption className="border-b border-slate-500 text-gray-800 text-[8px] mb-3">
+            <div className="border-l border-t border-r border-slate-600 grid grid-cols-10 gap-5 justify-center items-center p-[2px]">
                 {legend.english.map((item, index) => <div key={index} className="">{item.score}</div>)}           
             </div>
-            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center p-1">
+            <div className="border-l border-r border-t border-slate-600 grid grid-cols-10 gap-5 justify-center items-center p-1">
                 {legend.english.map((item, index) => <div key={index} className="">{item.name}</div>)}
             </div>
-            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center">
+            <div className="border-l border-r border-t border-slate-600 grid grid-cols-10 gap-5 justify-center items-center">
                 {legend.english.map((item, index) => <div key={index} className="">{item.description}</div>)}
             </div>
         </caption>
