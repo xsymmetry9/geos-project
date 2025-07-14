feb9c206e3e758fe053af223d32366518a5e51c9
xsymmetry9 <glswim@gmail.com>
2025-07-14 14:40:12 +0800

    Add bg color

--- frontend/src/components/PrintComponents/Legend.tsx
+++ frontend/src/components/PrintComponents/Legend.tsx
@@ -4,14 +4,14 @@ const Legend = () => {
     console.log(legend.english);
 
     return(
-        <caption className="border-l border-r border-slate-500 text-gray-800 text-[8px]">
-            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center p-[2px]">
+        <caption className="border-l border-r border-b border-slate-500 text-gray-800 text-[8px] mb-2">
+            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center p-[2px] bg-orange-100">
                 {legend.english.map((item, index) => <div key={index} className="">{item.score}</div>)}           
             </div>
-            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center p-1">
+            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center p-1 bg-white">
                 {legend.english.map((item, index) => <div key={index} className="">{item.name}</div>)}
             </div>
-            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center">
+            <div className="border-t border-slate-500 grid grid-cols-10 gap-5 justify-center items-center bg-orange-300">
                 {legend.english.map((item, index) => <div key={index} className="">{item.description}</div>)}
             </div>
         </caption>
