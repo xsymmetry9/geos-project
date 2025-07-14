27a384438462c51f2d17a1d0d919be1df9aca8c2
xsymmetry9 <glswim@gmail.com>
2025-07-14 18:27:19 +0800

    Add function

--- frontend/src/type/LevelCheckForm.ts
+++ frontend/src/type/LevelCheckForm.ts
@@ -1,22 +1,46 @@
 import {v4 as uuidv4} from 'uuid';
 
-class LevelCheck {
+class StrengthAndWeakness {
+  level_name: string;
+  strength: string;
+  weakness: string;
+
+  constructor(level_name = "", strength = "", weakness =""){
+    this.level_name = level_name;
+    this.strength = strength;
+    this.weakness =  weakness;
+  }
+}
+
+class LevelCheckEntry {
     id: string;
     dateCreated: Date;
-    name: string;
+    student_name: string;
+    teacher_name: string;
     feedback: string;
-    language_proficiency_level: string;
-    textbook: string;
+    speaking: StrengthAndWeakness;
+    confidence: StrengthAndWeakness;
+    grammar: StrengthAndWeakness;
+    vocabulary: StrengthAndWeakness;
+    listening: StrengthAndWeakness;
+    pronunciation: StrengthAndWeakness;
+
     constructor(id = uuidv4(), dateCreated = new Date()) {
       this.id = id;
       this.dateCreated = dateCreated;
-      this.name = "";
+      this.student_name = "";
+      this.teacher_name = "";
       this.feedback = "";
-      this.language_proficiency_level = "";
-      this.textbook = "";
+      this.speaking = new StrengthAndWeakness();
+      this.confidence = new StrengthAndWeakness();
+      this.grammar = new StrengthAndWeakness();
+      this.vocabulary = new StrengthAndWeakness();
+      this.listening = new StrengthAndWeakness();
+      this. pronunciation = new StrengthAndWeakness();
 
     }
   }
 
-  export default LevelCheck;
+  export {LevelCheckEntry, StrengthAndWeakness};
   
+
