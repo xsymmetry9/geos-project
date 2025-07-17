05d16cb346eb235743b30513c029077324b2113e
xsymmetry9 <glswim@gmail.com>
2025-07-17 17:33:28 +0800

    Chore

--- frontend/src/pages/LevelCheckForm.tsx
+++ frontend/src/pages/LevelCheckForm.tsx
@@ -14,7 +14,6 @@ const LevelCheckForm = () => {
     
   }, []);
 
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement> | ChangeEvent<HTMLTextAreaElement>) => {
     const {name, value} = e.currentTarget;
     setInputData((prev) => ({
@@ -30,26 +29,35 @@ const LevelCheckForm = () => {
       </div>
       <form autoComplete="off">
         <section>
-          <h3>Personal Information</h3>
-          {}
-          <label htmlFor="student_name"> Student Name
-            <input name="student_name" id="input-student_name" onChange={handleChange} type="text" />
-          </label>
-          <label htmlFor="feedback"> Feedback
-            <textarea name="feedback" onChange={handleChange} id="input-feeback" />
-          </label>
+          <div>
+            <label htmlFor="student_name"> Student Name
+              <input className="form-input font-primary text-base text-black mt-1 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:outline-0 focus:ring-0 focus:border-[#09c5eb] hover:border-[#09c5eb]" name="student_name" id="input-student_name" onChange={handleChange} type="text" />
+            </label>
+          </div>
         </section>
         <section>
         <LevelCheckSelect item="speaking" inputData ={inputData} setInputData={setInputData} />
-        {/* <LevelCheckSelect item="confidence" inputData ={inputData} setInputData={setInputData} />
+        <LevelCheckSelect item="confidence" inputData ={inputData} setInputData={setInputData} />
         <LevelCheckSelect item="grammar" inputData ={inputData} setInputData={setInputData} />
         <LevelCheckSelect item="vocabulary" inputData ={inputData} setInputData={setInputData} />
         <LevelCheckSelect item="pronunciation" inputData ={inputData} setInputData={setInputData} />
-        <LevelCheckSelect item="listening" inputData ={inputData} setInputData={setInputData} /> */}
+        <LevelCheckSelect item="listening" inputData ={inputData} setInputData={setInputData} />
         </section>
+        <section id="input-feedback">
+           <label htmlFor="feedback"> Feedback
+            <textarea className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-2 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#09c5eb] sm:text-sm/6" name="feedback"  onChange={handleChange} id="input-feeback" />
+          </label>
+        </section>
+        <div className="w-full flex justify-center pt-3">
+         <button className="btn-primary">
+            Submit
+          </button>
+        </div>
+   
        
       </form>
-      <div id="preview-level-check">
+
+      <div className="" id="preview-level-check">
         {error && <p className="text-red">{error}</p>}
         <p>Form id: {inputData.id}</p>
         <p>date: {inputData.dateCreated}</p>
