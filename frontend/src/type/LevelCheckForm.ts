4a771b5c134612bb79cb03bb370f48841f102983
xsymmetry9 <glswim@gmail.com>
2025-07-17 16:24:57 +0800

    Chore: change to array from string

--- frontend/src/type/LevelCheckForm.ts
+++ frontend/src/type/LevelCheckForm.ts
@@ -2,10 +2,10 @@ import {v4 as uuidv4} from 'uuid';
 
 class StrengthAndWeakness {
   level_name: string;
-  strength: string;
-  weakness: string;
+  strength: string[];
+  weakness: string[];
 
-  constructor(level_name = "", strength = "", weakness =""){
+  constructor(level_name = "", strength: string[] = [], weakness: string[] = []){
     this.level_name = level_name;
     this.strength = strength;
     this.weakness =  weakness;
@@ -14,9 +14,8 @@ class StrengthAndWeakness {
 
 class LevelCheckEntry {
     id: string;
-    dateCreated: Date;
+    dateCreated: string;
     student_name: string;
-    teacher_name: string;
     feedback: string;
     speaking: StrengthAndWeakness;
     confidence: StrengthAndWeakness;
@@ -25,11 +24,10 @@ class LevelCheckEntry {
     listening: StrengthAndWeakness;
     pronunciation: StrengthAndWeakness;
 
-    constructor(id = uuidv4(), dateCreated = new Date()) {
+    constructor(id = uuidv4()) {
       this.id = id;
-      this.dateCreated = dateCreated;
+      this.dateCreated = "";
       this.student_name = "";
-      this.teacher_name = "";
       this.feedback = "";
       this.speaking = new StrengthAndWeakness();
       this.confidence = new StrengthAndWeakness();
