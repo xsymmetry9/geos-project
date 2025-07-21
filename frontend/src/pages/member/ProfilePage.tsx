bd2410034a7cf8d85daa77dd7c0c4a0e60163463
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-07-21 11:41:46 +0800

    Add pages

--- frontend/src/pages/member/ProfilePage.tsx
+++ frontend/src/pages/member/ProfilePage.tsx
@@ -1,16 +1,137 @@
-import { useLocation } from "react-router-dom";
-const ProfilePage = () => {
-    const location = useLocation();
-    
-    const user = location.state;
-    
-    return(
-        <>
-            <h1 className="text-center font-bold text-3xl">Hello there, {user.name}</h1>
+import { useRef, useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-            
-        </>
-    )
-}
+const ProfilePage = () => {
+  const [error, setError] = useState("");
+  const [user, setUser] = useState<any>();
+  const [students, setStudents] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [selectedStudentId, setSelectedStudentId] = useState<string | null>(null);
+
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setError("Not authenticated");
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        const result = await axios.get("http://localhost:8000/api/profile", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        const loadData = result.data;
+        setUser(loadData.user);
+        setStudents(loadData.data);
+      } catch (err) {
+        console.error(err);
+        setError("There was an error loading the data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() =>{
+    const handleClickOutside = (event: MouseEvent) => {
+        if(dropdownRef.current && !dropdownRef.current.contains(event.target as Node)){
+            setSelectedStudentId(null);
+        } 
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return() => {
+        document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+  });
+
+  const toggleOptions = (id: string) => {
+    setSelectedStudentId((prev) => (prev === id ? null : id));
+  };
+
+  const handleEdit = (student: any) => {
+    console.log("Editing student:", student);
+    // Navigate to edit page or open modal
+  };
+
+  const handleDelete = async (id: string) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+
+      console.log(id);
+      const result = await axios.delete(`http://localhost:8000/api/member/deleteStudent/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setStudents((prev) => prev.filter((student) => student.id !== id));
+      console.log(result.data.message); 
+    } catch (err) {
+      console.error("Delete failed:", err);
+    }
+  };
+
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p className="text-red">{error}</p>;
+
+  return (
+    <>
+      <h1 className="text-center font-bold text-3xl">{user.name}!</h1>
+      <table className="border-t border-gray-300 mt-6 m-auto w-full max-w-[1100px]">
+        <thead>
+          <tr className="border-b border-gray-300">
+            <td className="p-2 font-bold">Name</td>
+            <td className="p-2 font-bold">Joined</td>
+            <td className="p-2 font-bold">Nickname</td>
+            <td className="p-2 font-bold">Email</td>
+            <td className="p-2 font-bold"></td>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((item) => (
+            <tr key={item.id} className="border-b border-gray-300 relative hover:bg-gray-200">
+              <td className="p-2">{item.name}</td>
+              <td className="p-2">{new Date(item.createdAt).toLocaleDateString()}</td>
+              <td className="p-2">{item.nickname}</td>
+              <td className="p-2">{item.email}</td>
+              <td className="p-2">
+                <div className="w-[30px] h-[30px] flex justify-center items-center hover:bg-gray-300 hover:rounded-full">
+                <button onClick={() => toggleOptions(item.id)} className="cursor-pointer bg-none text-slate-500 hover:underline">
+                  ...
+                </button>
+
+                </div>
+          
+              </td>
+
+              {selectedStudentId === item.id && (
+                <td colSpan={5} className="relative">
+                  <div
+                    ref={dropdownRef} 
+                    className="z-10 flex flex-col gap-2 w-[120px] p-2 bg-gray-100 border border-gray-300 mt-2 rounded gap-4 absolute top-0 right-[90px]">
+                    <Link to={`/profile/viewStudent/${item.id}`} className="cursor-pointer w-full text-center text-sm hover:underline hover:text-blue-600">View Student</Link>
+                    <Link to={`/spr/${item.id}`} className="cursor-pointer w-full text-center text-sm hover:underline hover:text-blue-600">SPR</Link>
+                    <Link to={`/levelCheck/${item.id}`} className="cursor-pointer w-full text-center text-sm hover:underline hover:text-blue-600">Level Check</Link>
+                    <Link to={`/profile/editStudent/${item.id}`} className="cursor-pointer w-full text-center text-sm hover:text-blue-600 hover:underline">Edit</Link>
+                    <button onClick={() => handleDelete(item.id)} className="cursor-pointer text-sm text-red-600 hover:underline">
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default ProfilePage;
--- frontend/src/pages/member/StudentPage.tsx
+++ frontend/src/pages/member/StudentPage.tsx
@@ -1,46 +1,182 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import {format} from "date-fns";
 
+type CreateInputProps = {
+        title: string;
+        type: string;
+        name: keyof typeof initialForm;
+        value: string;
+        error: string;
+        handle: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    }
+const initialForm = {name: "", nickname: "", email:""};
+
+const CreateInput = ({title, type, name, value, error, handle} : CreateInputProps) => {
+        return(
+             <>
+                <label htmlFor={name}>
+                    <span>{title}</span>
+                    <input 
+                        className="form-input font-primary text-bases text-black mt-1 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:outline-0 focus:ring-0 focus:border-[#09c5eb] hover:border-[#09c5eb]"
+                        type={type}
+                        name={name}
+                        value={value}
+                        id={name}
+                        onChange={handle}/>
+
+                    </label>
+        
+                {error && <p className="text-red-600 text-sm">{error}</p>}
+             </>
+        )
+    }
+export const StudentPage = () => {
+    let {id} = useParams();
+    const [formData, setFormData] = useState({id: "", name: "", email: "", nickname: "", createdAt: ""});
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+     useEffect(() => {
+            setLoading(true);
+            let token = localStorage.getItem("token");
+            if(!token) {
+                setErrorMessage("Not authenticated");
+                return; //Navigate to login?
+            }
+            
+            const fetchData = async () => {
+                try {
+                    const result = await axios.get(`http://localhost:8000/api/member/getStudentById/${id}`, {
+                        headers: { Authorization: `Bearer ${token}`},
+                    });
+                    const loadData = result.data.data;
+                    setFormData({
+                        id: loadData.id, 
+                        name: loadData.name, 
+                        email: loadData.email, 
+                        nickname: loadData.nickname, 
+                        createdAt: loadData.createdAt
+                    });
+
+                } catch(err) {
+                    console.log("Error:", err);
+                } finally {
+                    setLoading(false);
+                }
+            };
+        fetchData();
+      },[id]); 
+    return(
+        <div className="font-secondary w-full max-w-[1100px] m-auto relative">
+            {errorMessage && <div className="w-[120px] h-[40px] z-1 bg-white absolute top-0 left-1/2 translate-x-1/2 translate-y-1/2 flex justify-center items-center border border-slate-500"><p className="text-center">Error Message</p></div>}
+            <h1 className="font-secondary text-center text-2xl font-bold">Personal Information</h1>
+            <div className="mt-3 max-w-[500px] w-full mx-auto  text-gray-700">
+              <p>Name: {formData.name}</p>
+              <p>Email: {formData.email}</p>
+              <p>Date Created: {format(formData.createdAt, "MM/dd/yyyy")}</p>
+            </div>
+            <div className="mt-6 flex gap-3 justify-center">
+                <Link className= "btn btn-primary" to={`/profile/viewLevelCheck/${id}`}>View Level Check</Link>
+                <Link className= "btn btn-primary" to={`/profile/createSPR/${id}`}>Create a SPR</Link>
+                <Link className= "btn btn-primary" to={`/profile/editStudent/${id}`}>Edit Student</Link>
+            </div>
+            <div id="initial-level-table">
+
+            </div>
+        </div>
+    )
+}
 export const CreateStudent = () => {
-    const [form, setForm] = useState({name: "", language: ""});
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
+    const [formData, setFormData] = useState({name: "", nickname: "", email: ""});
+    const [error, setError] = useState({name: "Enter a name", nickname: "Enter a nickname", email: "Enter an email"});
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("")
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
-        console.log(`Name: ${form.name} Language: ${form.language}`);
+        
+        //Checks if there are any errors.
+        const hasErrors = Object.values(error).some((val) => val !== "");
+
+        if(hasErrors){
+            setErrorMessage("Incomplete form");
+            return;
+        } else {
+            setLoading(true);
+            try{
+                const token = localStorage.getItem("token");
+                
+                if(!token){
+                    setErrorMessage("Not authenticated");
+                    return;
+                }
+                const res = await axios.post("http://localhost:8000/api/member/addStudent", formData,
+                    {
+                        headers: {Authorization: `Bearer ${token}`}
+                    }
+                );
+                return res;
+            } catch (error: any)
+            {
+                if(error.response?.status === 404){
+                   setErrorMessage("Failed to connect to the backend");
+                } else {
+                    setErrorMessage("Error");
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        axios
     }
 
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setForm((prevForm) => ({...prevForm, [name]: value}));
-    }
-    return(
-        <div className="w-full static max-w-lg m-auto">
+        const {name, value} = e.currentTarget;
+
+        if (name === "email" && value && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value))
+            // /^ ... $/ Starts in a string (/^) and ends in a string ($/)
+            // [^\s@]+ 
+            {
+                setError((prev) => ({...prev, email: 'Invalid email format'}));
+            } else {
+                setError((prev) => ({
+                    ...prev, 
+                    [name]: value === "" ? `Enter a ${name}` : ""
+                }));
+            }
+
+        setFormData((prevFormData) => ({...prevFormData, [name]: value}));
         
+    };
+
+    useEffect(() => {
+        if(errorMessage){
+            const timer = setTimeout(() => {
+                setErrorMessage("")
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+      }, [errorMessage])
+
+    return(
+        <div className="w-full static max-w-lg m-auto relative">
+            {errorMessage && <div className="w-[120px] h-[40px] z-1 bg-white absolute top-0 left-1/2 translate-x-1/2 translate-y-1/2 flex justify-center items-center border border-slate-500"><p className="text-center">Error Message</p></div>}
+
             <h1 className="font-secondary text-center text-2xl font-bold">Create A student</h1>
             <div className="text-gray-700 capitalize">
-                <form onClick={handleSubmit}>
-                    <label htmlFor="name">
-                        <span>Name</span>
-                    </label>
-                    <input 
-                        className="form-input font-primary text-base text-black mt-1 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:outline-0 focus:ring-0 focus:border-[#09c5eb] hover:border-[#09c5eb]"
-                        type="text" 
-                        name="name" 
-                        onChange={handleInput}/>
-                    <label htmlFor="language">
-                        <span>Language</span>
-                    </label>
-                    <input 
-                        className="form-input font-primary text-base text-black mt-1 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:outline-0 focus:ring-0 focus:border-[#09c5eb] hover:border-[#09c5eb]"
-                        type="text" 
-                        name="language" 
-                        onChange={handleInput}/>
-
+                <form autoComplete ="off" onSubmit={handleSubmit}>
+                    <CreateInput title={"Name"} type={"text"} name={"name"} value={formData.name} error = {error.name} handle={handleInput}/>
+                    <CreateInput title={"Nickname"} type={"text"} name={"nickname"} value={formData.nickname} error= {error.nickname} handle={handleInput}/>
+                    <CreateInput title={"Email"} type={"email"} name={"email"} value={formData.email} error= {error.email} handle={handleInput}/>
+   
                     <div className="flex gap-2 justify-center mt-3" >
-                        <input 
+                        {!loading && <input 
                             className="btn btn-primary"
                             type="submit" 
-                            value={"Done"} 
-                            onChange={handleInput}/>
+                            value={"Save"} />}
+                        {loading && <p>Loading ...</p>}
                     </div>
          
                 </form>
@@ -49,10 +185,133 @@ export const CreateStudent = () => {
     )
 }
 
-export const ViewStudents = () =>{
-    return(
-        <>
-            <h1>View all students</h1>
-        </>
-    )
-}
+export const EditStudent = () => {
+    let {id} = useParams();
+    const [formData, setFormData] = useState({id: "", name: "", nickname: "", email: "", createdAt: ""});
+    const [error, setError] = useState({id: "", name: "", nickname: "", email: "", createdAt: ""});
+    const [errorMessage, setErrorMessage] = useState("");
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        
+        //Checks if there are any errors.
+        const hasErrors = Object.values(error).some((val) => val !== "");
+
+        if(hasErrors){
+            setErrorMessage("Incomplete form");
+            return;
+        } else {
+            setLoading(true);
+            try{
+                const token = localStorage.getItem("token");
+                
+                if(!token){
+                    setErrorMessage("Not authenticated");
+                    return;
+                }
+                const res = await axios.put(`http://localhost:8000/api/member/updateStudent/${formData.id}`, formData,
+                    {
+                        headers: {Authorization: `Bearer ${token}`}
+                    }
+                );
+                return res;
+            } catch (error:any)
+            {
+                if(error.response?.status === 404){
+                   setErrorMessage("Failed to connect to the backend");
+                } else {
+                    setErrorMessage("Error");
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+    }
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.currentTarget;
+
+        if (name === "email" && value && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value))
+            // /^ ... $/ Starts in a string (/^) and ends in a string ($/)
+            // [^\s@]+ 
+            {
+                setError((prev) => ({...prev, email: 'Invalid email format'}));
+            } else {
+                setError((prev) => ({
+                    ...prev, 
+                    [name]: value === "" ? `Enter a ${name}` : ""
+                }));
+            }
+
+        setFormData((prevFormData) => ({...prevFormData, [name]: value}));
+    }
+
+        
+    useEffect(() => {
+            setLoading(true);
+            let token = localStorage.getItem("token");
+            if(!token) {
+                setErrorMessage("Not authenticated");
+                return; //Navigate to login?
+            }
+            
+            const fetchData = async () => {
+                try {
+                    const result = await axios.get(`http://localhost:8000/api/member/getStudentById/${id}`, {
+                        headers: { Authorization: `Bearer ${token}`},
+                    });
+                    const loadData = result.data.data;
+                    setFormData({
+                        id: loadData.id, 
+                        name: loadData.name, 
+                        email: loadData.email, 
+                        nickname: loadData.nickname, 
+                        createdAt: loadData.createdAt
+                    });
+
+                } catch(err) {
+                    setErrorMessage("Error");
+                } finally {
+                    setLoading(false);
+                }
+            };
+        fetchData();
+      },[id]); 
+
+      useEffect(() => {
+        if(errorMessage){
+            const timer = setTimeout(() => {
+                setErrorMessage("")
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+      }, [errorMessage])
+      return(
+         <div className="w-full static max-w-lg m-auto">
+            {errorMessage && <div className="w-[120px] h-[40px] z-1 bg-white absolute top-0 left-1/2 translate-x-1/2 translate-y-1/2 flex justify-center items-center border border-slate-500"><p className="text-center">Error Message</p></div>}
+            <h1 className="font-secondary text-center text-2xl font-bold">Edit student</h1>
+            <div className="text-gray-700 capitalize">
+                <form autoComplete ="off" onSubmit={handleSubmit}>
+                    <CreateInput title={"Name"} type={"text"} name={"name"} value={formData.name} error = {error.name} handle={handleInput}/>
+                    <CreateInput title={"Nickname"} type={"text"} name={"nickname"} value={formData.nickname} error= {error.nickname} handle={handleInput}/>
+                    <CreateInput title={"Email"} type={"email"} name={"email"} value={formData.email} error= {error.email} handle={handleInput}/>
+   
+                    <div className="flex gap-2 justify-center mt-3" >
+                        {!loading && (
+                            <>
+                               <input 
+                                className="btn btn-primary"
+                                type="submit" 
+                                value={"Save"}/>
+                                <Link to="/profile/viewStudents" className="btn btn-primary">Cancel</Link>
+                            </>
+                         )}
+                        {loading && <p>Loading ...</p>}
+                    </div>
+         
+                </form>
+            </div>
+        </div>
+    );
+};
