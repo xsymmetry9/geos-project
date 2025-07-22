a6d752bd3a8a800e1a017e18aaf749ff31af8268
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-07-22 11:43:04 +0800

    Chore

--- backend/sprSeed.js
+++ backend/sprSeed.js
@@ -5,6 +5,27 @@ const prisma = new PrismaClient();
 
 async function main() {
 
+  const createForm = (obj) => {
+    const {studentId} = obj;
+    console.log(studentId);
+
+
+  }
+
+  const readForm = () => {
+
+  }
+
+  const updateForm = () => {
+
+  }
+
+  const deleteForm = () => {
+
+  }
+
+  createForm({studentId: "d22097a7-4bcb-4e3c-bb8b-5a57fa7e3981"});
+
 }
 
 main()
