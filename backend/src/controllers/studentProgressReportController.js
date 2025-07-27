c7545ae78da28dc3995d7d76ef6ef45010629fd7
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-07-27 10:22:43 +0800

    Add CRUD functions

--- backend/src/controllers/studentProgressReportController.js
+++ backend/src/controllers/studentProgressReportController.js
@@ -1,28 +1,75 @@
-const {create} = require("../services/sprService");
-const createStudentProgressReport = async (req, res) => {
-    const studentId = req.body.studentId;
-    const teacherEmail = req.data.email;
-    const data = req.body.data;
-
-    console.log(studentId, teacherEmail);
-
-    console.log("The data is:", data);
-
-  
+const {create, findOneByFormId, findAll, editAll} = require("../services/sprService");
+// Create
+const createStudentProgressReport = async (req, res) => { 
     try{
-        const createForm = await create({studentId: studentId, teacherEmail: teacherEmail, data: data});
-
+        const createForm = await create(req, res);
         res.send({status: true, message: "it worked!", data: createForm});
         return;
 
     } catch (error)
     {
         console.error("Error:", error);
+        res.send({status: false, message: "It didn't work", error: error});
         return;
+    }
+}
+// Read
+const getStudentReport = async(req, res) => {
+    try{
+        const formID = req.params.formID
+        const form = await findOneByFormId(formID);
+
+        return res.send(form);
+    } catch (error) {
+        return res.status(500).json({error: "Error on the server side"});
+
     }
 
 }
 
+const getAllStudentReportsByStudentID = async (req, res) => {
+    try{
+        const form = await findAll(req);
+        return res.status(200).json(form);
+    } catch (error) {
+        return res.status(500).json({error: "Error on the server side"});
+    }
+}
+
+// Update
+
+const updateAllDataStudentReportByFormID = async (req, res) => {
+    try{
+        //Checks if formId exist
+
+        const data = req.body.data;
+        const {formId} = req.body.data;
+
+        const existForm = await findOneByFormId(formId)
+        if(!existForm) {
+            throw new Error("It doesn't exist");
+        } 
+        
+        const updatedData = await editAll(formId, data);
+    
+
+        // return res.status(200).json(form);
+
+        return res.status(200).json(updatedData);
+    } catch(error) {
+        return res.status(500).json({error:"Error on the server side"});
+    }
+}
+
+const deleteFormByFormID = async (req, res) => {
+    try {
+        const deleted = await deleteFormByFormID(req);
+        return res.status(204).json(deleted);
+    } catch (error) {
+        return res.status(500).json({error: "Error on the server side"});
+    }
+}
+
 module.exports = {
-    createStudentProgressReport
+    createStudentProgressReport, getStudentReport, getAllStudentReportsByStudentID, updateAllDataStudentReportByFormID, deleteFormByFormID
 }
