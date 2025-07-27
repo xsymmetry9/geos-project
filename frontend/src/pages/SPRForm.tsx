de3ce5208b3e9ccaded8fe9e682b30760f3cf447
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-07-27 10:26:26 +0800

    src/pages/SPRForm.tsx

--- frontend/src/pages/SPRForm.tsx
+++ frontend/src/pages/SPRForm.tsx
@@ -1,16 +1,13 @@
 import { useState, createContext, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import PlotForm from "../components/Form/PlotForm";
 import { StudentProgressReportEntry } from "../type/StudentProgressReportEntry";
-// import { getStudentById } from "../utils/functions";
-import { House } from "lucide-react";
-import axios from "axios";
-import TeacherPage from "./Admin/TeacherPage";
 
 export const LanguageContext = createContext<string>("english");
 
 const SPRForm = () => {
-  let navigate = useNavigate();
+  const location = useLocation();
+  const formData = location.state;
   let parameters = useParams();
   const preferedLanguage = parameters.language ? parameters.language : "english";
   const studentId = parameters.id
@@ -20,50 +17,22 @@ const SPRForm = () => {
   const [inputData, setInputData] = useState<StudentProgressReportEntry>(initialStudent); //Creates an new or edit form
   const [loading, setLoading] = useState<boolean>(false);
 
+  
   useEffect(() => {
     setInputData((prev) => ({
       ...prev,
+      formId: formData.formId,
+      name: formData.studentName,
       studentId: studentId ?? "",
+      teacherEmail: formData.teacherEmail,
       language: preferedLanguage
     }));
-  },[studentId, language]);
-  //This is for auto save... not sure if i want this
-    // useEffect(() => {
-      
-    //   const token = localStorage.getItem("token");
-    //   if(!token) {
-    //       console.error("No token, login first to create a new token");
-    //       return;
-    //     }
+  },[studentId, language, formData]);
 
-    //   const fetchId = async ()=> {
-    //       setLoading(true);    
-    //       try{
-    //          const res = await axios.post(`http://localhost:8000/api/member/createSPR/${studentId}`,
-    //           {studentId: studentId},
-    //           {
-    //             headers: { Authorization: `Bearer ${token}`},
-    //           });
-
-    //         if(res.data?.data){
-    //           setInputData(res.data.data);
-    //         }
-    
-    //         return res;
-    //       } catch(error){
-    //         console.error("Error was found", error);
-    //         return;
-    //       } finally {
-    //           setLoading(false);
-    //         };
-    //       };
-          
-    //       fetchId();
-    //     }, [studentId]);
   return (
     <LanguageContext.Provider value={language}>
       {!loading && 
-        <PlotForm inputData={inputData} setInputData={setInputData} setLoading ={setLoading} />
+        <PlotForm inputData={inputData} setInputData={setInputData} loading = {loading} setLoading ={setLoading} />
       }
     </LanguageContext.Provider>
 
