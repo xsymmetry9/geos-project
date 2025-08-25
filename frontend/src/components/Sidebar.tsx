2ee4d7b11c97d105a43313782dcf4af325d5818a
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-08-25 10:20:14 +0800

    Update to navlink

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import LogoutButton from "./LogoutButton"
 
 const Sidebar = () =>{
     return (
         <nav className="h-full flex flex-col gap-4 p-4">
-            <Link to="/profile/viewStudents" className="text-gray-700 hover:text-blue-600">View Students</Link> 
-            <Link to="/profile/createStudent" className="text-gray-700 hover:text-blue-600">Add Student</Link>            
-            <Link to="/profile/setting" className="text-gray-700 hover:text-blue-600">Settings</Link>            
+            <NavLink to="/profile/viewStudents" className="text-gray-700 hover:text-blue-600">View Students</NavLink> 
+            <NavLink to="/profile/createStudent" className="text-gray-700 hover:text-blue-600">Add Student</NavLink>            
+            <NavLink to="/profile/setting" className="text-gray-700 hover:text-blue-600">Settings</NavLink>            
             <LogoutButton />          
         </nav>       
     )
