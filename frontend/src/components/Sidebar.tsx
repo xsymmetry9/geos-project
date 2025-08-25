d0df6deac15abdfd5f7d59b8001ed7c6a2964e25
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-08-25 10:13:22 +0800

    Chore: update sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import LogoutButton from "./LogoutButton"
 
 const Sidebar = () =>{
     return (
         <nav className="h-full flex flex-col gap-4 p-4">
-            <a href="/profile/viewStudents" className="text-gray-700 hover:text-blue-600">View Students</a> 
-            <a href="/profile/createStudent" className="text-gray-700 hover:text-blue-600">Add Student</a>            
-            <a href="/profile/setting" className="text-gray-700 hover:text-blue-600">Settings</a>            
+            <Link to="/profile/viewStudents" className="text-gray-700 hover:text-blue-600">View Students</Link> 
+            <Link to="/profile/createStudent" className="text-gray-700 hover:text-blue-600">Add Student</Link>            
+            <Link to="/profile/setting" className="text-gray-700 hover:text-blue-600">Settings</Link>            
             <LogoutButton />          
         </nav>       
     )
