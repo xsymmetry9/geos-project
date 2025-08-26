ae2909275c51d08d6a3cdce58fa3754a445e72a3
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-08-26 22:27:07 +0800

    Chore: update file

--- backend/start.sh
+++ backend/start.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-
-set -e # Exit on error
-
-export PATH="./node_modules/.bin:$PATH"
 # Generate Prisma client code
 npx prisma generate
-prisma db push 
+
+# Sync schema to Neon (safe and idempotent)
+npx prisma db push
 
 # Start your Express app
 node src/server.js
