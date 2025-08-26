22684f9fe3a249796322bc81d44ffb911ec16d9b
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-08-26 19:58:55 +0800

    Chore

--- backend/start.sh
+++ backend/start.sh
@@ -5,7 +5,7 @@ set -e # Exit on error
 export PATH="./node_modules/.bin:$PATH"
 # Generate Prisma client code
 npx prisma generate
-npm run migrate:deploy
+prisma db push 
 
 # Start your Express app
 node src/server.js
