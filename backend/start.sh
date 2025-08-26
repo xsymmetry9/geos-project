08d47e7fd05a8ebdca1284d56f76870c58279fc6
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-08-26 11:35:02 +0800

    Add CI/CD

--- backend/start.sh
+++ backend/start.sh
@@ -2,8 +2,12 @@
 
 set -e # Exit on error
 
+export PATH="./node_modules/.bin:$PATH"
 # Generate Prisma client code
 npx prisma generate
+npm run migrate:deploy
+
+npm run seed:admin
 
 # Sync schema to Neon (safe and idempotent)
 npx prisma db push
