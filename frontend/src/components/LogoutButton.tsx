360d1d074c62b913b17e0d2bcabf05c8c1e48866
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-08-27 11:45:56 +0800

    Refacto

--- frontend/src/components/LogoutButton.tsx
+++ frontend/src/components/LogoutButton.tsx
@@ -1,3 +1,4 @@
+import { LogOut } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 const LogoutButton = () => {
@@ -15,8 +16,8 @@ const LogoutButton = () => {
     return(
         <button 
             onClick={handleLogout}
-            className="mt-4 bg-red-600 hover:bg-red-700 text-white font-semi-bold py-2 px-4 rounded">
-            Log Out
+            className="cursor-pointer w-32 mt-4 bg-red-600 flex items-center hover:bg-red-700 text-white font-semi-bold py-2 px-4 rounded">
+                <LogOut size="small" color="white" className="w-[30px] h-[30px] mr-2" /><span>Log out</span>
             </button>
     )
 }
