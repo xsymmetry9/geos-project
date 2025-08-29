6ce0140ad27dd826bb4f6cdf6f9141b323c01b5f
gProgrammer <glswim@gmail.com>
2025-08-29 16:07:26 +0800

    Seperate components to smaller ones

--- frontend/src/components/PrintSPR.tsx
+++ frontend/src/components/PrintSPR.tsx
@@ -1,69 +1,17 @@
-import { useState, useEffect, useRef, useMemo, useCallback } from "react";
-import { useReactToPrint } from "react-to-print";
+import { useRef, useMemo } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getStudentById } from "../utils/functions";
 import {PrintContent} from "./PrintStudentProgressReport";
-import html2canvas from "html2canvas-pro";
-import jsPDF from "jspdf";
+import PrintControl from "./PrintControl";
+import SaveControl from "./SaveControl";
 
 const PrintPage = () => {
   const { id, language } = useParams<{id: string; language: string}>(); //Gets id and language through link
   const componentRef = useRef<HTMLDivElement>(null); //Save reference to print
-  const [isPrinting, setIsPrinting] = useState(false);
-  const promiseResolveRef = useRef<null | (()=> void)>(null);
-
   const parsedData = useMemo(()=> {
     if (!id) return null;
     return getStudentById(id); //Gets data from localstorage by id
   }, [id]);
-
-  useEffect(() => {
-    if (isPrinting && promiseResolveRef.current) {
-      promiseResolveRef.current();
-    }
-  }, [isPrinting]);
-
-  const reactToPrintContent = useCallback(() => componentRef.current, []);
-
-  const handlePrint = useReactToPrint({
-    content: reactToPrintContent,
-    documentTitle: "Student Progress Report",
-    onBeforePrint: () => 
-       new Promise<void>((resolve) => {
-        promiseResolveRef.current = resolve;
-        setIsPrinting(true);
-      }),
-    onAfterPrint: () => {
-      promiseResolveRef.current = null;
-      setIsPrinting(false);
-    },
-  });
-
-
-  const handleGeneratePDF = async () => {
-  if (!componentRef.current) return;
-
-  const canvas = await html2canvas(componentRef.current, {
-    allowTaint: true,
-    scale: 2,
-    useCORS: true,
-    backgroundColor: "#ffffff",
-  });
-
-  const imgData = canvas.toDataURL("image/png");
-
-  const pdf = new jsPDF("p", "mm", "a4");
-  const pdfWidth = pdf.internal.pageSize.getWidth();
-  const pdfHeight = pdf.internal.pageSize.getHeight();
-
-  // Convert canvas to image scaled for A4
-  const imgProps = pdf.getImageProperties(imgData);
-  const imgRatio = imgProps.width / imgProps.height;
-  const height = pdfWidth / imgRatio;
-
-  pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, height);
-  pdf.save(`student-report-${id}.pdf`);
-};
 
   if(!parsedData) return <div>Loading ...</div>
 
@@ -79,13 +27,9 @@ const PrintPage = () => {
         <PrintContent parsedData={parsedData} />
       </div>
     </div>
-    
-  
       <div className="flex justify-center pt-3 gap-3">
-        <button className="btn btn-primary print w-[150px]" onClick={() => handlePrint(reactToPrintContent)}>
-          Print
-        </button>
-        <button className="btn btn-primary print" onClick={handleGeneratePDF}>Save as PDF</button>
+        <PrintControl contentRef={componentRef} className="btn btn-primary print" />
+        <SaveControl contentRef = {componentRef} className = "btn btn-primary print" />
       </div>
     </>
   );
