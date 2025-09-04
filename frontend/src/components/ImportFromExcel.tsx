9534f5c89b6d2248275ba7faa9a6f635a5688368
gProgrammer <glswim@gmail.com>
2025-09-04 17:23:36 +0800

    Chore: work on importing data from Excel

--- frontend/src/components/ImportFromExcel.tsx
+++ frontend/src/components/ImportFromExcel.tsx
@@ -1,22 +1,75 @@
 import React, { useCallback } from "react";
 import { read, utils } from "xlsx";
 import { Student, Levels } from "../type/Student";
+import { LevelCheckEntry, StrengthAndWeakness } from "@/type/LevelCheckForm";
 import { editDataFromLocal, getDataFromLocal } from "../utils/functions";
-import { Files, Upload } from "lucide-react";
+import { Upload } from "lucide-react";
 import User from "@/type/User";
+
+type Category = "confidence" | "speaking" | "listening" | "grammar" | "pronunciation" | "vocabulary";
 
 type ImportFromExcelProps = {
   userData: User;
   setUserData: React.Dispatch<React.SetStateAction<User | null>>;
 };
 
-const ImportFromExcel: React.FC<ImportFromExcelProps> = ({ userData, setUserData }) => {
-  const { SPR } = userData;
-  console.log(userData);
+const ParseLevelCheckEntry = (sheet: any) => {
+  const jsonData: any[] = utils.sheet_to_json(sheet);
 
-  const mergedArrayFilterUniqueObjects = useCallback((arr1: Student[], arr2: Student[]) => {
+  let entries: LevelCheckEntry[] = [];
+  jsonData.forEach((row) => {
+    const entry = new LevelCheckEntry();
+    entry.id = row.id;
+    entry.student_name = row.student_name;
+    entry.bookRecommendation = row.bookRecommendation;
+    entry.feedback = row.feedback;
+    entry.overallCEFR = row.overallCEFR;
+
+    const categories: Category[] = ["confidence", "speaking", "listening", "grammar", "pronunciation", "vocabulary"];
+    categories.forEach((cat: Category) => {
+      const category = entry[cat];
+      category.level_name = row[`${cat}_level`];
+      category.score = row[`${cat}_score`];
+      row[`${cat}_strengths`].split("; ").forEach((item: any) =>category.strength.push(item));
+      row[`${cat}_weakness`].split("; ").forEach((item: any) => category.weakness.push(item));
+    })
+    entries.push(entry);
+  });
+
+  return entries;
+}
+
+const parseSPREntry = (sheet: any) => {
+  const jsonData: any[] = utils.sheet_to_json(sheet);
+
+  let students: Student[] = [];
+  jsonData.forEach((row) => {
+    const student = new Student(row.id);
+    student.dateCreated = row.Date || "";
+    student.name = row.Name || "";
+    student.course = row.Course || "No course name";
+    student.textbook = row.Textbook || "No textbook";
+    student.attendance = Number(row.Attendance || 0);
+    student.totalLessons = Number(row.TotalLessons || 0);
+    student.feedback = row.Feedback || "No feedback";
+    student.levels = {
+      vocabulary: new Levels(String(row.Vocabulary_initial), String(row.Vocabulary_target), String(row.Vocabulary_final)),
+      pronunciation: new Levels(String(row.Pronunciation_initial), String(row.Pronunciation_target), String(row.Pronunciation_final)),
+      grammar: new Levels(String(row.Grammar_initial), String(row.Grammar_target), String(row.Grammar_final)),
+      listening: new Levels(String(row.Listening_initial), String(row.Listening_target), String(row.Listening_final)),
+      conversation: new Levels(String(row.Conversation_initial), String(row.Conversation_target), String(row.Conversation_final)),
+    };
+    students.push(student);
+  })
+  return students;
+}
+
+const ImportFromExcel: React.FC<ImportFromExcelProps> = ({ userData, setUserData }) => {
+  const { SPR, levelCheck } = userData;
+
+  const mergedArrayFilterUniqueObjects = useCallback((arr1: any[], arr2: any[]) => {
     const mergedArray = [...arr1, ...arr2];
-    const uniqueObjects = new Map<string, Student>();
+    const uniqueObjects = new Map<string, any>();
     mergedArray.forEach((item) => {
       uniqueObjects.set(item.id, item);
     });
@@ -35,43 +88,40 @@ const ImportFromExcel: React.FC<ImportFromExcelProps> = ({ userData, setUserData
     reader.onload = (event) => {
       try {
         const arrayBuffer = event.target?.result;
+
         if (!arrayBuffer || !(arrayBuffer instanceof ArrayBuffer)) return;
 
         const data = new Uint8Array(arrayBuffer);
         const workbook = read(data, { type: "array" });
 
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const jsonData: any[] = utils.sheet_to_json(worksheet);
+        let arrOfSheetNames: string[] = [];
+        workbook.SheetNames.forEach((item) => arrOfSheetNames.push(item));
 
-        const parsedStudents: Student[] = jsonData.map((row) => {
-          const student = new Student(row.id);
-          student.dateCreated = row.Date || "";
+        arrOfSheetNames.forEach((item) => {
+          if (item === "Teacher's data"){
+            const worksheet = workbook.Sheets[item];
+            const parsedData = parseSPREntry(worksheet);
 
-          student.name = row.Name || "";
-          student.course = row.Course || "No course name";
-          student.textbook = row.Textbook || "No textbook";
-          student.attendance = Number(row.Attendance || 0);
-          student.totalLessons = Number(row.TotalLessons || 0);
-          student.feedback = row.Feedback || "No feedback";
+            const result = mergedArrayFilterUniqueObjects(parsedData, SPR);
+            const updatedUser = { ...userData, SPR: result };
+            editDataFromLocal(updatedUser);
+            setUserData(updatedUser);
+            
+          } else if (item === "Level Check"){
+            const worksheet = workbook.Sheets[item];
+            const parsedData = ParseLevelCheckEntry(worksheet);
 
-          student.levels = {
-            vocabulary: new Levels(String(row.Vocabulary_initial), String(row.Vocabulary_target), String(row.Vocabulary_final)),
-            pronunciation: new Levels(String(row.Pronunciation_initial), String(row.Pronunciation_target), String(row.Pronunciation_final)),
-            grammar: new Levels(String(row.Grammar_initial), String(row.Grammar_target), String(row.Grammar_final)),
-            listening: new Levels(String(row.Listening_initial), String(row.Listening_target), String(row.Listening_final)),
-            conversation: new Levels(String(row.Conversation_initial), String(row.Conversation_target), String(row.Conversation_final)),
-          };
+            const result = mergedArrayFilterUniqueObjects(parsedData, levelCheck);
+            const updatedUser = { ...userData, levelCheck: result };
+            editDataFromLocal(updatedUser);
+            setUserData(updatedUser);
 
-          return student;
-        });
+            console.log(parsedData);
+          } else {
+            return;
+          }
+        })
 
-        const result = mergedArrayFilterUniqueObjects(parsedStudents, SPR);
-
-        // Update localStorage and component state
-        const updatedUser = { ...userData, SPR: result };
-        editDataFromLocal(updatedUser);
-        setUserData(updatedUser);
       } catch (err: any) {
         alert(err.message || "An error occurred during file upload");
       }
