36366c9997c835bc5c6c1c7f265b18ea9b699f3c
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-10-19 00:09:26 +0800

    Change color background

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const date = new Date().getFullYear();
 
   return (
-    <footer className="w-full bg-[#00646c] text-white font-primary text-sm py-4">
+    <footer className="w-full bg-gray-600 text-white font-primary text-sm py-4">
       <div className="max-w-[1100px] mx-auto px-4 flex flex-col items-center gap-1 text-center">
         <p>&copy; GEOS Online Language Academy. All Rights Reserved.</p>
         <p>Created by Gary – {date}</p>
